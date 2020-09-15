d2537d8e840a44a65bb338246eaba80efbe23501
robbidarwis22 <robbidarwis22@gmail.com>
2020-09-15 21:09:50 +0700

    SplashScreen

---
+++ app/src/main/java/com/example/kotlinuberremake/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinuberremake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+            .subscribe({
+                Toast.makeText(this@SplashScreenActivity,"Splash Screen run done!",Toast.LENGTH_SHORT).show()
+            })
+    }
+}
